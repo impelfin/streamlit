75e4daffe749816333d74cb54b7a353815a46dbf
JunHyeon Mun <impelfin@gmail.com>
2024-06-02 18:59:47 +0900

    up

--- sidebar/app.py
+++ sidebar/app.py
@@ -28,4 +28,4 @@ image_files = [
 # 셀렉트박스에서 선택한 항목에 따라 이미지 표시
 selectbox_options_index = selectbox_options.index(your_option) # selectbox_options의 리스트 인덱스 찾기
 image_file = image_files[selectbox_options_index] # 선택한 항목에 맞는 이미지 파일 지정
-st.image(image_file, caption=image_file)        # 이미지 표시
+st.image(image_file, caption=your_option)        # 이미지 표시
