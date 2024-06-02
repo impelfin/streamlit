ed0a09236e29dfdce3e1ebb9f34c5d990f7eff92
JunHyeon Mun <impelfin@gmail.com>
2024-06-02 18:29:14 +0900

    up

--- sidebar/app.py
+++ sidebar/app.py
@@ -17,21 +17,11 @@ st.sidebar.write('**당신의 선택**:', your_option)
 # ---------- 메인(Main) 화면 구성 --------------------
 st.title("스트림릿의 사이드바 사용 예")
 
-# folder = './data'
-
 # selectbox_options의 요소에 따라서 보여줄 이미지 파일 리스트(selectbox_options의 요소와 순서를 일치시킴)
-# image_files = ['Vermeer.png', 'Gogh.png', 'Munch.png', 'ShinYoonbok.png'] # 이미지 파일 리스트
-image_files = [
-    'https://github.com/impelfin/streamlit/blob/main/data/Vermeer.png', 
-    'https://github.com/impelfin/streamlit/blob/main/data/Gogh.png', 
-    'https://github.com/impelfin/streamlit/blob/main/data/Munch.png', 
-    'https://github.com/impelfin/streamlit/blob/main/data/ShinYoonbok.png'
-    ] # 이미지 파일 리스트
+image_files = ['Vermeer.png', 'Gogh.png', 'Munch.png', 'ShinYoonbok.png'] # 이미지 파일 리스트
 
 # 셀렉트박스에서 선택한 항목에 따라 이미지 표시
 selectbox_options_index = selectbox_options.index(your_option) # selectbox_options의 리스트 인덱스 찾기
 image_file = image_files[selectbox_options_index] # 선택한 항목에 맞는 이미지 파일 지정
-image_url = image_file 
-# st.image(image_url, caption=your_option)        # 이미지 표시
-# st.image(image_url, caption=image_url)        # 이미지 표시
-st.image('https://github.com/impelfin/project/data/Vermeer.png')
+image_local = Image.open(image_file)     # Image.open() 함수로 이미지 파일 열기
+st.image(image_local, caption=your_option)        # 이미지 표시
