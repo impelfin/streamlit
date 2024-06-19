22ea822f0a6faf788ec4f550d502366ef7727aa6
impelfin <impelfin@gmail.com>
2024-06-19 06:57:11 +0000

    up

--- sidebar/app.py
+++ sidebar/app.py
@@ -17,8 +17,10 @@ st.sidebar.write('**당신의 선택** : ', your_option)
 folder = './data/'
 
 image_files = [
-    'Vermeer.png', 'Gogh.png', 'Munch.png', 'ShinYoonbok.png'
-    
+    'https://github.com/impelfin/streamlit/blob/main/sidebar/data/Vermeer.png?raw=true', 
+    'https://github.com/impelfin/streamlit/blob/main/sidebar/data/Gogh.png?raw=true', 
+    'https://github.com/impelfin/streamlit/blob/main/sidebar/data/Munch.png?raw=true', 
+    'https://github.com/impelfin/streamlit/blob/main/sidebar/data/ShinYoonbok.png?raw=true'
     ]
 
 selectbox_options_index = selectbox_options.index(your_option)
